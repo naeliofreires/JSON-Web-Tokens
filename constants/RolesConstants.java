57923c27cecba7181cc1ea977678095012c548e6
naeliofreires <naeliosistemas@gmail.com>
2018-05-30 20:02:38 -0300

    .

---
+++ constants/RolesConstants.java
@@ -0,0 +1,7 @@
+package br.com.app.tanamao.constants;
+
+public class RolesConstants {
+    public static final String ADM = "ROLE_ADM";
+    public static final String USER = "ROLE_USER";
+    public static final String DRUG = "ROLE_DRUGSTORE";
+}
